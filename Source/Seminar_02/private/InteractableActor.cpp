b019676e31b9861484bb35a80da62a9e4fedf96b
Liza Kiva <LizaKKiva@yandex.ru>
2026-09-21 12:23:45 +0300

    feat(code): add interaction objects

---
+++ Source/Seminar_02/private/InteractableActor.cpp
@@ -0,0 +1,23 @@
+﻿#include "../public/InteractableActor.h"
+
+AInteractableActor::AInteractableActor() {
+  
+  PrimaryActorTick.bCanEverTick = false;
+
+  InteractionName = TEXT("InteractableActor");
+}
+
+void AInteractableActor::Interact_Implementation() {
+  UE_LOG(
+    LogTemp,
+    Warning,
+    TEXT("Default Interaction %s"),
+    *InteractionName);
+
+  OnInteracted.Broadcast();
+}
+
+
+
+
+
---
+++ Source/Seminar_02/public/InteractableActor.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "InteractableActor.generated.h"
+
+//DECLARE_DELEGATE(FOnInteracted);
+//DECLARE_MULTICAST_DELEGATE(FOnInteracted);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnInteracted);
+
+
+UCLASS()
+class SEMINAR_02_API AInteractableActor : public AActor {
+  GENERATED_BODY()
+
+public:
+  AInteractableActor();
+
+  UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Interaction")
+  void Interact();
+
+  UPROPERTY(BlueprintAssignable, Category = "Interaction")
+  FOnInteracted OnInteracted;
+
+protected:
+
+  virtual void Interact_Implementation();
+
+  UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Interaction")
+  FString InteractionName;
+  
+};
